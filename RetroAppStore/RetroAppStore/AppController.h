e69cbd898197817a00f8c3f40426551cc7139f8d
Minh Ton <ford.tonthat@gmail.com>
2020-04-30 13:47:22 +0700

    RetroAppStore

--- RetroAppStore/RetroAppStore/AppController.h
+++ RetroAppStore/RetroAppStore/AppController.h
@@ -13,7 +13,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface AppController : NSObject {
-@private
+@private 
     IBOutlet NSTextField *progresslabel;
     IBOutlet NSProgressIndicator *progressBar;
     IBOutlet NSButton *continuebutton;
